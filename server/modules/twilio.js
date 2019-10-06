9f0028898079155661e267af650c345b45a55db9
Erin Verbeck <erin@merginglanes.net>
2019-10-06 05:52:12 -0500

    add receiveMessage func for Twilio to respond to the user

--- server/modules/twilio.js
+++ server/modules/twilio.js
@@ -15,7 +15,26 @@ function sendReminder(username, appointmentName, url){
     .then(message => console.log(message.sid));
   }
 
-  function receiveMessage()
+function receiveMessage(){
+  const http = require('http');
+  const express = require('express');
+  const MessagingResponse = require('twilio').twiml.MessagingResponse;
+
+  const app = express();
+
+  app.post('/sms', (req, res) => {
+    const twiml = new MessagingResponse();
+
+    twiml.message('Thanks for scheduling your appointment!');
+
+    res.writeHead(200, {'Content-Type': 'text/xml'});
+    res.end(twiml.toString());
+  });
+
+  http.createServer(app).listen(1337, () => {
+    console.log('Express server listening on port 1337');
+  });
+}
 
   module.exports = {
     sendReminder
