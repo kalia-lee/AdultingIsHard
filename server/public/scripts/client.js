4732ed978afb7b18a974f097bacd61b56281fb82
Erin Verbeck <erin@merginglanes.net>
2019-10-06 11:19:55 -0500

    add in twilio SO IT ACTUALLY WORKS :)

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,7 +1,5 @@
 console.log('client.js connected');
 
-const twilio = require('../../modules/twilio');
-
 var btn = document.getElementById('getNames');
 var allNames = document.getElementById('allNames');
 var url = "http://localhost:5000/names";
@@ -16,10 +14,33 @@ async function getNamesFromDB(){
   console.log('response: ', text);
 };
 
+async function triggerTwilioServerSide(){
+  console.log('Gonna go twilio');
+  var twilioUrl = "http://localhost:5000/goGoTwilio";
 
+  var response = await fetch(twilioUrl, { method: 'POST'});
+  var text = await response;
+  console.log('twilio response: ', text);
+};
+
+/*
 function triggerTwilio() {
-  twilio.sendReminder("Grace", "oil change", "URL_GOES_HERE")
-}
+  let username = "Grace"
+  let appointmentName = "oil change"
+  let url = "URL_GOES_HERE"
+    const accountSid = config.twilioSID;
+    const authToken = config.twilioAuthToken;
+    const client = require('twilio')(accountSid, authToken);
+    let messageText = "Hi, " + username + " this is Automate My Life checking in! Just wanted to remind you to schedule your " + appointmentName + " appointment soon. When you've scheduled your appointment, respond back with OIL. Click here to schedule your appointment: " + url
+
+    client.messages
+      .create({
+         body: "messageText",
+         from: config.twilioPhoneSend,
+         to: config.twilioMyPhone
+       })
+      .then(message => console.log(message.sid));
+}*/
 // async function checkDueDate() {
 //   let today = new Date();
 //   var response = await fetch('http://localhost:5000/events'); //Pull events from Database
